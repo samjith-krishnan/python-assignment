b64690c20d674a57878454865ca81ca8602d3bdb
samjith <samjithkrishna7510@gmail.com>
2026-07-21 19:51:41 +0530

    completed part 1 loading data and printing

--- src/analyzer.py
+++ src/analyzer.py
@@ -14,13 +14,52 @@ class OHLCVAnalyzer:
         self.file_path = Path(file_path)
         self.df = None
 
-    def load_data(self):
-        """Load the dataset."""
-        pass
+    def load_data(self) -> None:
+        """
+        Load the OHLCV dataset and parse timestamps.
+        """
+        self.df = pd.read_csv(
+            self.file_path,
+            parse_dates=["timestamp"]
+    )
 
-    def validate_data(self):
-        """Validate dataset."""
-        pass
+        self.df.sort_values("timestamp", inplace=True)
+        self.df.reset_index(drop=True, inplace=True)
+
+        print(" Dataset loaded successfully.")
+
+
+
+    def validate_data(self) -> None:
+        """
+        Validate the dataset by checking missing values,
+        duplicate timestamps, and displaying a summary.
+        """
+
+        print("\n" + "=" * 50)
+        print("DATA VALIDATION")
+        print("=" * 50)
+
+
+        print("\nMissing Values:")
+        print(self.df.isnull().sum())
+
+    
+        duplicate_count = self.df["timestamp"].duplicated().sum()
+        print(f"\nDuplicate timestamps: {duplicate_count}")
+
+  
+        print("\nDataset Info:")
+        self.df.info()
+
+
+        print("\nStatistical Summary:")
+        print(self.df.describe())
+
+        print("\nFirst 5 Rows:")
+        print(self.df.head())
+
+        
 
     def calculate_indicators(self):
         """Calculate SMA, returns and volatility."""
--- src/main.py
+++ src/main.py
@@ -1,3 +1,23 @@
+# from analyzer import OHLCVAnalyzer
+
+
+# def main():
+#     analyzer = OHLCVAnalyzer("../data/sample_ohlcv_data.csv")
+
+#     analyzer.load_data()
+#     analyzer.validate_data()
+#     analyzer.calculate_indicators()
+#     analyzer.detect_trend()
+#     analyzer.analyze()
+#     analyzer.plot()
+#     analyzer.save()
+
+
+# if __name__ == "__main__":
+#     main()
+
+
+
 from analyzer import OHLCVAnalyzer
 
 
@@ -6,11 +26,6 @@ def main():
 
     analyzer.load_data()
     analyzer.validate_data()
-    analyzer.calculate_indicators()
-    analyzer.detect_trend()
-    analyzer.analyze()
-    analyzer.plot()
-    analyzer.save()
 
 
 if __name__ == "__main__":
